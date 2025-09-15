bc7d9cba655332e39c11b4ed304c15c78815e788
alikhazaie2018-code <alikhazaie2018@gmail.com>
2025-09-15 18:43:06 +0330

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.post("/data", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
 
 
